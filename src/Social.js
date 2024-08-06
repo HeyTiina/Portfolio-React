0164f3308da7d727da6cdcdd98e395224cdaf164
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-08-06 21:27:55 +0200

    icons better

--- src/Social.js
+++ src/Social.js
@@ -1,43 +1,49 @@
 import React from "react";
+import linkedinIcon from "./IconsSocial/linkedin.png";
+import instagramIcon from "./IconsSocial/instagram.png";
+import githubIcon from "./IconsSocial/github.png";
 import "./Social.css";
 
-import Linkedin from "./IconsSocial/linkedin.png";
-import Instagram from "./IconsSocial/instagram.png";
-import GitHub from "./IconsSocial/github.png";
+const socialMedia = [
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/valentina-bastidas-castillo-654652266/",
+    icon: linkedinIcon,
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/hey.tiina/",
+    icon: instagramIcon,
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/HeyTiina",
+    icon: githubIcon,
+  },
+];
 
-export default function Social() {
-  return (
-    <section className="social">
-      <h1>Let's get in touch!</h1>
-      <h3>
-        {" "}
-        I'm always looking for new opportunities. Feel free to contact me!
-      </h3>
-      <div className="grid-container">
-        <a
-          href="https://www.linkedin.com/in/valentina-bastidas-castillo-654652266/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="social-icons" src={Linkedin} alt="Linkedin" />
-        </a>
+const SocialMediaItem = ({ socialMedia }) => (
+  <a href={socialMedia.url} target="_blank" rel="noopener noreferrer">
+    <img
+      src={socialMedia.icon}
+      alt={socialMedia.name}
+      className="social-icons"
+    />
+  </a>
+);
 
-        <a
-          href="https://www.instagram.com/hey.tiina/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="social-icons" src={Instagram} alt="Instagram" />
-        </a>
+const Social = () => (
+  <section className="social">
+    <h1>Let's get in touch!</h1>
+    <h3> I'm always looking for new opportunities. Feel free to contact me!</h3>
+    <div className="grid-container-social">
+      {socialMedia.map((socialMediaItem) => (
+        <div key={socialMediaItem.name} className="grid-item">
+          <SocialMediaItem socialMedia={socialMediaItem} />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-        <a
-          href="https://github.com/HeyTiina"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="social-icons" src={GitHub} alt="Github" />
-        </a>
-      </div>
-    </section>
-  );
-}
+export default Social;
