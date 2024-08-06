01f6ac5f8d6c4efe203a9030322172ea453123e1
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-08-06 22:05:55 +0200

    about.css added

--- src/About.js
+++ src/About.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./About.css";
 
 export default function About() {
   return (
     <section>
       <h1>About me</h1>
-      <p className="description">
+      <p className="about">
         I graduated last year and am ready to take on new challenges by bringing
         extensive work-under-pressure experience in catering services from
         Mallorca, Spain. I am a hardworking team player looking to join a
