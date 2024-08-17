b2346f8d44cb9e0ade241bcc415389dd395286ed
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-08-17 20:22:18 +0200

    bones in about

--- src/About.js
+++ src/About.js
@@ -1,11 +1,63 @@
 import React from "react";
 import "./About.css";
+import Valentina from "./Valentina.jpg";
 
 export default function About() {
   return (
     <section className="about">
       <h1 className="title">About me</h1>
-      
+      <div>
+        <div className="container">
+          <div className="location special-border">
+            <img
+              className="Valentina-profile"
+              src={Valentina}
+              alt="Valentina Bastidas C."
+            />{" "}
+            <div>Born in Cali, Colombia</div> <br></br>
+            <div className="text-location">
+              Developed a love for boardgames, videogames, rol games and every
+              other word with the word ‘’games’’ included.
+            </div>
+          </div>
+          <div className="location">
+            <img
+              className="Valentina-profile"
+              src={Valentina}
+              alt="Valentina Bastidas C."
+            />{" "}
+            <div>Raised in Mallorca, Spain</div> <br></br>
+            <div className="text-location">
+              Worked in catering services next to my studies I have developed
+              exceptional skills as:{" "}
+              <ul>
+                <li>working under pressure</li>
+                <li>adaptability</li>
+                <li>quick learning</li>
+                <li>problem-solving</li>
+              </ul>{" "}
+              Being surrunded by my friends and their interest in IT, they
+              helped to build my own interest in it!
+            </div>
+          </div>
+          <div className="location">
+            <img
+              className="Valentina-profile"
+              src={Valentina}
+              alt="Valentina Bastidas C."
+            />{" "}
+            <div>Living in zürich, Switzerland</div> <br></br>
+            <div className="text-location">
+              Moved to Switzerland, I though that this was the time to make a
+              change and enrolled in some online courses.
+              <div className="mt-4">
+                The journey has been great, a learned a lot and I have
+                still a lot more to learn.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
